4a6dc593bc0ab86cb2f3cab48dcdcd6468ac8ff2
dominik.schwarzbauer <dominik.schwarzbauer@gmail.com>
2014-09-18 09:42:34 +0000

    + Config Funktioniert nun * Code wurde aufgeräumt und ausgelagert

---
+++ Config.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Downloader
+{
+    public class Config
+    {
+        private string configPath = Environment.CurrentDirectory; // ausführpfad
+        private ConfigType type;
+
+        public Config(ConfigType type)
+        {
+            this.type = type;
+        }
+
+        public string ReadConfig()
+        {
+            try
+            {
+                if (type == ConfigType.DownloadConfig)
+                {
+                    string path = configPath + "\\config_downloader.cfg";
+                    int line = (int)ConfigType.DownloadConfig;
+                    string[] content = File.ReadAllLines(path);
+
+                    // clean up
+                    path = null;
+
+                    if (!Directory.Exists(content[line]))
+                        Directory.CreateDirectory(content[line]);
+
+                    return content[line];
+                }
+                else
+                    return null;
+            }
+            catch (Exception e)
+            {
+                Console.Clear();
+                Console.WriteLine(e.Message);
+                return null;
+            }
+        }
+
+        public bool CorrectMediaType(string tmp)
+        {
+            try
+            {
+                // eingabe wie folgt: mp3;mp4;wav;
+                string path = configPath + "\\config_mediafiles.cfg";
+                string files = File.ReadAllText(path);
+
+                string[] tmp_ending = tmp.Split('.');
+                string ending = "." + tmp_ending[tmp_ending.Length - 1];
+                ending = ending.Replace("\"", "");
+
+                if (files.Contains(ending))
+                    return true;
+                else
+                    return false;
+            }
+            catch (Exception e)
+            {
+                Console.Clear();
+                Console.WriteLine(e.Message);
+                return false;
+            }
+        }
+
+        public void SetConfigType(ConfigType type)
+        {
+            this.type = type;
+        }
+    }
+
+    public enum ConfigType
+    {
+        DownloadConfig
+    }
+}
---
+++ Downloader.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Downloader
+{
+    public class Downloader
+    {
+        // Programm Variablen
+        private Config config = new Config(ConfigType.DownloadConfig); //initiale Config kann zur Laufzeit geändert werden für andere werte
+
+
+        // Download Variablen
+        private string pfad = ""; //wird durch Config() gesetzt.
+        private string leerzeichen = " ";
+        private string Ordner = null;
+        private string Speicherpfad = null;
+        private string Speicherpfad_neu = null;
+
+        public Downloader()
+        {
+            pfad = config.ReadConfig();
+            Run();
+        }
+
+        /// <summary>
+        /// Aktiviert die Downloadroutine!
+        /// </summary>
+        private void Run()
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Bitte geben Sie die zu durchsuchende URL ein: \n(Qick Edit Modus ist an)");
+
+            string url = Console.ReadLine();
+
+            Console.WriteLine("Leerzeichen kodiert als: (zuerst leer lassen, manche Server verwenden ein +)");
+            leerzeichen = Console.ReadLine();
+
+            if (leerzeichen == " ")
+                Console.WriteLine("Leerzeichen als Standard gespeichert.");
+            else
+                Console.WriteLine("Leerzeichen als {0} dargestellt", leerzeichen);
+
+            Thread.Sleep(700); // fancy
+
+            Console.WriteLine("Speicherpfad: " + pfad);
+
+            Thread.Sleep(1000); // fancy
+
+            Console.WriteLine("Speicherpfad erweitern:  (falls kein Über-Ordner gewünscht ist, bitte Enter drücken)");
+            Ordner = Console.ReadLine();
+
+            if (Ordner.EndsWith(@"/"))
+                Ordner = Ordner.TrimEnd('/');
+
+            if (!Ordner.EndsWith(@"\"))
+                Ordner += @"\";
+
+            Console.WriteLine("Speicherpfad: " + pfad + Ordner);
+            Speicherpfad = pfad + Ordner;
+
+            string dir = "";
+
+            GetFilesFromHTTP http = new GetFilesFromHTTP(this);
+            http.HoleDateien(url, dir, "");
+        }
+
+        public string GetLeerzeichen()
+        {
+            return leerzeichen;
+        }
+
+        public string GetSpeicherpfad()
+        {
+            return Speicherpfad;
+        }
+
+        public string GetNewSpeicherpfad()
+        {
+            return Speicherpfad_neu;
+        }
+
+        public void Restart()
+        {
+            Run();
+        }
+
+        public Config GetConfig()
+        {
+            return config;
+        }
+    }
+}
