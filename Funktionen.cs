faede9a12bf83ea0f88d0275837d45643e496eb2
Engelmayer93@gmail.com <Engelmayer93@gmail.com@5a4c9ae2-ad06-5955-6236-281075e69b41>
2014-09-20 21:40:38 +0000

    Buffer überlauf hoffentlich gelöst ;)

--- Funktionen.cs
+++ Funktionen.cs
@@ -5,19 +5,35 @@ using System.Text;
 
 namespace Downloader
 {
+    
     class Funktionen
     {
+       
 
         public static void Lösche_Zeile()
         {
             Console.WriteLine("                                                                                                                                                                               ");
         }
 
-        public static void Zeile_Farbig(string text, ConsoleColor color)
+        //public static void Zeile_Farbig(string text, ConsoleColor color)   (noch) nicht notwendig?
+        //{
+        //    Console.ForegroundColor = color;
+        //    Console.WriteLine(text);
+        //    Console.ForegroundColor = Config.standard;
+        //}
+
+
+        public static void Buffer_höhe_erhöhen(int zahl)  // Wichtig, dass das Prog. bei einem "überlauf" des Buffers noch richtig funzt
         {
-            Console.ForegroundColor = color;
-            Console.WriteLine(text);
-            Console.ForegroundColor = Config.standard;
+            try
+            {
+                Console.SetBufferSize(Console.BufferWidth, Console.BufferHeight + zahl);
+            }
+            catch (Exception e)
+            {
+                Console.Clear();
+                Console.WriteLine(e.ToString());
+            }
         }
 
     }
